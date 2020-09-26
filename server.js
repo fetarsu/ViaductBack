f9a422db506255efb4721f56bba17847398e8561
Rafał <s16849@pjwstk.edu.pl>
2020-09-26 20:36:00 +0200

    dodanie tabel

---
+++ server.js
@@ -0,0 +1,12 @@
+var app = require('express')(); // Create an instance of an Express app
+
+var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
+
+mobileApp.tables.add('User');
+mobileApp.tables.add('Deliverer');
+mobileApp.tables.add('Employee');
+mobileApp.tables.add('Operations');
+mobileApp.tables.add('Report'); // Create a table for 'User' with default settings
+
+app.use(mobileApp);
+app.listen(process.env.PORT || 3000);
