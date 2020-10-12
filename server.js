31576dfd2b55bf6a17ab8dc290d2e866f783dff6
Rafał <s16849@pjwstk.edu.pl>
2020-10-12 18:52:09 +0200

    dodanie dwóch tabel

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ mobileApp.tables.add('Deliverer');
 mobileApp.tables.add('Employee');
 mobileApp.tables.add('Operation');
 mobileApp.tables.add('Report'); // Create a table for 'User' with default settings
-
+mobileApp.tables.add('Adress');
+mobileApp.tables.add('Supply');
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
